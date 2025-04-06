078a463fa03aa6240885b5bc42a7c90467bb0f59
Chetan Krishna Palicherla <chetanpalicherla@gmail.com>
2025-04-05 22:06:04 -0400

    Set up for data analysis

---
+++ data_analysis_asdiv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from data_process import get_tasks_asdiv, filter_tasks
+from completion import run_task_solo, run_task_omni
+
+def get_price(row):
+    solo_correct, solo_cost, solo_sol = run_task_solo({"Question": row["Question"], "Answer": row["Answer"]}, verbose=True)
+    omni_correct, omni_cost, omni_sol = run_task_omni({"Question": row["Question"], "Answer": row["Answer"]}, verbose=True)
+    
+    return solo_cost, omni_cost, solo_correct, omni_correct
+
+
+print("Loading tasks...")
+asdiv_tasks = get_tasks_asdiv()
+
+print("Converting tasks to DataFrame...")
+asdiv_df = pd.DataFrame(asdiv_tasks)
+
+print("Getting the number of words in each question...")
+asdiv_df['no_of_words'] = asdiv_df['Question'].apply(lambda x: len(x.split()))
+
+print("Calculating costs and correctness...")
+asdiv_df['solo_cost'], asdiv_df['omni_cost'], asdiv_df['solo_correct'], asdiv_df['omni_correct'] = zip(*asdiv_df.apply(get_price, axis=1))
+
+print("Saving results to CSV...")
+asdiv_df.to_csv('asdiv_analysis.csv', index=False)  # Save the dataframe to a CSV file
+
---
+++ data_analysis_gsm.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from data_process import get_tasks_gsm_8k, filter_tasks
+from completion import run_task_solo, run_task_omni
+
+def get_price(row):
+    solo_correct, solo_cost, solo_sol = run_task_solo({"Question": row["Question"], "Answer": row["Answer"]}, verbose=True)
+    omni_correct, omni_cost, omni_sol = run_task_omni({"Question": row["Question"], "Answer": row["Answer"]}, verbose=True)
+    
+    return solo_cost, omni_cost, solo_correct, omni_correct
+
+
+print("Loading tasks...")
+gsm_8k_tasks = get_tasks_gsm_8k()
+
+print("Converting tasks to DataFrame...")
+gsm_8k_df = pd.DataFrame(gsm_8k_tasks)
+
+print("Getting the number of words in each question...")
+gsm_8k_df['no_of_words'] = gsm_8k_df['Question'].apply(lambda x: len(x.split()))
+
+print("Calculating costs and correctness...")
+gsm_8k_df['solo_cost'], gsm_8k_df['omni_cost'], gsm_8k_df['solo_correct'], gsm_8k_df['omni_correct'] = zip(*gsm_8k_df.apply(get_price, axis=1))
+
+print("Saving results to CSV...")
+gsm_8k_df.to_csv('gsm_8k_analysis.csv', index=False)  # Save the dataframe to a CSV file
+
